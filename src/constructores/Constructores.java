0be74cb55c81a1a848e1ea14305088960b1f6c00
Tu Nombre <tu_correo@uniempresarial.edu.co>
2026-08-29 05:25:29 -0500

    Agregar constructor vacio con valores por defecto

--- src/constructores/Constructores.java
+++ src/constructores/Constructores.java
@@ -4,4 +4,8 @@ public class Persona {
     private String nombre;
     private int edad;
     private String documento;
-}
+}    public Persona() {
+        this.nombre = "Sin nombre";
+        this.edad = 0;
+        this.documento = "Sin documento";
+    }
